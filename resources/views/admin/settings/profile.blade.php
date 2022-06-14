b9289ebc9c7bf46d174774449ccea3e889e8d6bf
Farrelrk <farrerk4@gmail.com>
2022-06-14 15:05:23 +0700

    styling profile section

--- resources/views/admin/settings/profile.blade.php
+++ resources/views/admin/settings/profile.blade.php
@@ -2,8 +2,29 @@
 @section('title', 'Profile')
 @section('content')
     <section>
-        <div class="card">
-            <h1>Profile</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12 col-sm-12 col-s-2">
+                                    <div class="img-profile">
+                                        <img src="https://picsum.photos/200" class="rounded-circle" alt="gambar">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="profile">
+                                        <h3>Farrel Rafiardi</h3>
+                                        <h4>farrelrk4@gmail.com</h4>
+                                        <h4>Admin</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
