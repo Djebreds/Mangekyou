0f19fd38c918d565c0d0387dc7de459cfbaabf11
Djebreds <refiahmad.djebreds@gmail.com>
2022-06-15 15:36:53 +0700

    mengubah desain form profile setting

--- resources/views/admin/settings/account-setting.blade.php
+++ resources/views/admin/settings/account-setting.blade.php
@@ -3,7 +3,14 @@
 @section('content')
     <section>
         <header>
-
+            <h1>Profile Setting</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Profile</li>
+                    <li class="breadcrumb-item active" aria-current="page">Profile Setting</li>
+                </ol>
+            </nav>
         </header>
         <div class="container">
             <div class="row my-2">
@@ -11,10 +18,10 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-sm-12 col-s-2">
+                                <div class="col-sm-12 col-s-1">
                                     <div class="img-profile text-md-end text-center ">
                                         <img src="https://picsum.photos/200" class="rounded-circle img-thumbnail"
-                                            alt="gambar">
+                                            alt="gambar" style="">
                                     </div>
                                 </div>
                                 <div class="col-md-2 col-sm-12">
@@ -36,29 +43,49 @@
                         <div class="card-body">
                             <h3>Setting</h3>
                             <form>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                                <label for="exampleInputEmail1" class="form-label">Name</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            person
+                                        </span>
+                                    </div>
                                     <input type="email" class="form-control form-control-sm" id="exampleInputEmail1"
                                         aria-describedby="emailHelp">
                                     <div id="emailHelp" class="form-text"></div>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Email address</label>
+                                <label for="exampleInputPassword1" class="form-label">Email address</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            mail
+                                        </span>
+                                    </div>
                                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Phone Number</label>
+                                <label for="exampleInputPassword1" class="form-label">Phone Number</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            call
+                                        </span>
+                                    </div>
                                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="role" class="form-label">Role</label>
-                                    <input type="role" class="form-control form-control-sm" id="role">
+                                <label for="role" class="form-label">Role</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            manage_accounts
+                                        </span>
+                                    </div>
+                                    <input type="role" class="form-control form-control-sm" id="role" disabled>
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Address</label>
                                     <textarea type="password" class="form-control form-control-sm" id="exampleInputPassword1"></textarea>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-green">Change</button>
                             </form>
                         </div>
                     </div>
@@ -69,19 +96,34 @@
                         <div class="card-body">
                             <h3>Change Password</h3>
                             <form>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Old Password</label>
+                                <label for="exampleInputPassword1" class="form-label">Old Password</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            lock
+                                        </span>
+                                    </div>
                                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">New Password</label>
+                                <label for="exampleInputPassword1" class="form-label">New Password</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            enhanced_encryption
+                                        </span>
+                                    </div>
                                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
                                 </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                                <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                                <div class="mb-3 input-group">
+                                    <div class="input-group-text">
+                                        <span class="material-symbols-outlined">
+                                            lock_reset
+                                        </span>
+                                    </div>
                                     <input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
                                 </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-green ">Change</button>
                             </form>
                         </div>
                     </div>
--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 
     <link rel="stylesheet" href="{{ asset('css/admin-style/app.css') }}">
@@ -95,26 +97,26 @@
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 
     <script src="{{ asset('js/admin-script/app.js') }}"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#myTable').DataTable({});
         });
     </script>
 
     <script
-            src="https://cdn.jsdelivr.net/combine/npm/echarts@5.3.2/renderers.min.js,npm/echarts@5.3.2/features.min.js,npm/echarts@5.3.2/core.min.js">
+        src="https://cdn.jsdelivr.net/combine/npm/echarts@5.3.2/renderers.min.js,npm/echarts@5.3.2/features.min.js,npm/echarts@5.3.2/core.min.js">
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
-            integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
+        integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js"
-            integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous">
+        integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous">
     </script>
 </body>
 
